910ce4900697114cac60a336da2fa0d773c3b936
Kobus Viljoen <kobusv@gmail.com>
2017-10-25 22:28:33 +0200

    added exercise1 for reading files

---
+++ exercise/exercise1/exercise.py
@@ -0,0 +1,9 @@
+def get_file_lines(loc):
+    infile = open(loc, "r")
+    content = infile.readlines()
+    infile.close()
+    return [i.rstrip("\n") for i in content]
+
+lines = get_file_lines("./fruits.txt")
+for line in lines:
+    print(line)
